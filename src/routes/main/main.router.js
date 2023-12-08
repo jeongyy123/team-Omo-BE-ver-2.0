9de9c6025b0a003920f2f6b2370e4128ff7e1de2
jeongyy123 <jeongyy123@kakao.com>
2023-12-08 19:01:32 +0900

    feat: first commit

---
+++ src/routes/main/main.router.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { prisma } from "../../utils/prisma/index.js";
+git
+const router = express.Router();
+
+/* 인기글 조회 */
+router.get('')
+
+/* 최신글 조회 */
+
+
+
+/* 댓글 조회 */
+
+export default router;
