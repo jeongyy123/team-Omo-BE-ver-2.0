f8a09f5ac213f131d8458e48b2d92f8307437273
heyjk2212 <jinakim2212@gmail.com>
2023-12-11 16:39:52 +0900

    Fix - uncomment lines of code

--- src/app.js
+++ src/app.js
@@ -3,17 +3,16 @@ import UsersRouter from "./routes/users/user.router.js";
 import AuthRouter from "./routes/users/auth.router.js";
 // import ProfileRouter from "./routes/users/profile.router.js";
 import MainRouter from "./routes/main/main.router.js";
-import PostsRouter from './routes/posts/posts.router.js'
 import ProfileRouter from "./routes/users/profile.router.js";
-import PostsRouter from './routes/posts/posts.router.js'
+import PostsRouter from "./routes/posts/posts.router.js";
 import cookieParser from "cookie-parser";
-import ErrorMiddleware from './middlewares/error.middleware.js'
+import ErrorMiddleware from "./middlewares/error.middleware.js";
 import session from "express-session";
 // import configurePassport from "./passport/index.js";
 // import passport from "passport";
 import cors from "cors";
 import dotenv from "dotenv";
-import redis from 'redis';
+// import redis from "redis";
 
 dotenv.config();
 
@@ -22,12 +21,10 @@ const PORT = 5000;
 
 //* Redis 연결
 // redis[s]://[[username][:password]@][host][:port][/db-number]
-const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-  legacyMode: true, // 반드시 설정 !!
-});
-
-
+// const redisClient = redis.createClient({
+//   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+//   legacyMode: true, // 반드시 설정 !!
+// });
 
 // express-session을 passport 설정 전에 먼저 사용하도록 설정
 // app.use(
@@ -61,18 +58,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/api", [UsersRouter, PostsRouter]);
 
-redisClient.on('connect', () => {
-  console.info('Redis connected!');
-});
+// redisClient.on("connect", () => {
+//   console.info("Redis connected!");
+// });
 
-redisClient.on('error', (err) => {
-  console.error('Redis Client Error', err);
-});
+// redisClient.on("error", (err) => {
+//   console.error("Redis Client Error", err);
+// });
 
-redisClient.connect().then(); // redis v4 연결 (비동기)
-const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
+// redisClient.connect().then(); // redis v4 연결 (비동기)
+// const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
 
-app.use(ErrorMiddleware)
+app.use(ErrorMiddleware);
 app.use("/api", [ProfileRouter]);
 app.use("/auth", [UsersRouter, AuthRouter]);
 
