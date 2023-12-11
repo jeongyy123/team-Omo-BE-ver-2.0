57e192cf5700d179338c40f89d6ba292b431977a
kyeongminRoh <rudalstpa11@nate.com>
2023-12-11 18:01:10 +0900

    restart

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import UsersRouter from "./routes/users/user.router.js";
-// import AuthRouter from "./routes/users/auth.router.js";
+import AuthRouter from "./routes/users/auth.router.js";
 // import ProfileRouter from "./routes/users/profile.router.js";
 import MainRouter from "./routes/main/main.router.js";
 import ProfileRouter from "./routes/users/profile.router.js";
@@ -14,7 +14,7 @@ import session from "express-session";
 // import passport from "passport";
 import cors from "cors";
 import dotenv from "dotenv";
-import redis from 'redis';
+// import redis from 'redis';
 
 dotenv.config();
 
@@ -23,10 +23,10 @@ const PORT = 5000;
 
 //* Redis 연결
 // redis[s]://[[username][:password]@][host][:port][/db-number]
-const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-  legacyMode: true, // 반드시 설정 !!
-});
+// const redisClient = redis.createClient({
+//   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
+//   legacyMode: true, // 반드시 설정 !!
+// });
 
 
 
@@ -66,16 +66,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/api", [UsersRouter, PostsRouter]);
 
-redisClient.on('connect', () => {
-  console.info('Redis connected!');
-});
+// redisClient.on('connect', () => {
+//   console.info('Redis connected!');
+// });
 
-redisClient.on('error', (err) => {
-  console.error('Redis Client Error', err);
-});
+// redisClient.on('error', (err) => {
+//   console.error('Redis Client Error', err);
+// });
 
-redisClient.connect().then(); // redis v4 연결 (비동기)
-const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
+// redisClient.connect().then(); // redis v4 연결 (비동기)
+// const redisCli = redisClient.v4; // 기본 redisClient 객체는 콜백기반인데 v4버젼은 프로미스 기반이라 사용
 
 app.use(ErrorMiddleware)
 app.use("/api", [ProfileRouter]);
