b82f21abfa6b25d61a1fae72613bf2021a9a4c05
jeongyy123 <jeongyy123@kakao.com>
2023-12-15 22:45:47 +0900

    fix: 메인페이지 district없을때 전체 조회 추가

--- src/routes/locations/location.router.js
+++ src/routes/locations/location.router.js
@@ -1,64 +1,130 @@
 import express from "express";
 import { prisma } from "../../utils/prisma/index.js";
+import haversine from "haversine";
+import multer from "multer";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import dotenv from "dotenv";
+import { getManyImagesS3 } from "../../utils/getImageS3.js"
 // import authMiddleware from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+dotenv.config();
 
-// 모든 위치 (둘러보기)
-router.get("/locations", async (req, res) => {
-  const { latitude, longitude } = req.query;
-  const location = await prisma.locations.findMany(
-    res.status(200).json({ data: location }),
-  );
+const bucketName = process.env.BUCKET_NAME;
+const region = process.env.BUCKET_REGION;
+const accessKeyId = process.env.ACCESS_KEY;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId,
+    secretAccessKey,
+  },
+  region,
 });
 
-//특정 위치 조회 (인기게시글 순서)
-router.get("/posts/:locationId", async (req, res) => {
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-  // 거리순 점과 점 사이 거리 구하기
-  const { locationId, latitude, longitude } = req.params;
+// 모든 위치 (둘러보기)
+router.get("/locations", async (req, res, next) => {
+  try {
+    const { latitude, longitude } = req.query;
+    const { districtName } = req.query;
 
-  const p1 = { latitude, longitude }
-  const p2 = await prisma.locations.findMany({
-    where: { district },
-    select: { latitude, longitude }
-  })
-  function district(p1, p2) {
-    return Math.sqrt(Math.pow(p2.latitude - p1.latitude, 2) + Math.pow(p2.longitude - p1.longitude, 2))
-  }
-
-
-
-  const location = await prisma.locations.findMany({
-    select: {
-      Location: {
+    // 위치 정보 가져오기
+    const location = await prisma.locations.findMany({
+      where: { District: { districtName: districtName } },
+      select: {
         address: true,
         latitude: true,
         longitude: true,
-      },
-      Posts: {
-        select: {
-          content: true,
-          imgUrl: true,
-          likeCount: true,
-          starAvg: true,
-          creatredAt: true
+        Posts: {
+          select: {
+            imgUrl: true,
+            starAvg: 5
+          },
+          take: 1,
+          skip: 1,
         },
-        orderBy: {
-          likeCount: 'desc'
+        Category: {
+          select: {
+            categoryName: true,
+          },
         },
       },
-      Categories: {
-        select: {
-          categoryName: true
-        }
-      }
-    }
-  })
-  return res.status(200).json({ data: location })
-})
+    });
 
+    console.log("여기예요 여기", location)
+
+    // 거리 계산 및 정렬
+    if (latitude && longitude) {
+
+      const start = {
+        latitude: +latitude,
+        longitude: +longitude,
+      };
+
+      const locationsWithDistance = location
+        .map((loc) => {
+          return {
+            ...loc,
+            distance: haversine(
+              start,
+              { latitude: loc.latitude, longitude: loc.longitude },
+              { unit: "meter" },
+            ),
+          };
+        })
+        .sort((a, b) => a.distance - b.distance);
+
+      const imgUrlsArray = locationsWithDistance.flatMap((location) =>
+        location.Posts.map((post) => post.imgUrl),
+      );
+
+      console.log("imgUrlsArray", imgUrlsArray)
+
+
+      const paramsArray = imgUrlsArray.map((url) => ({
+        Bucket: bucketName,
+        Key: url,
+      }));
+
+      const signedUrlsArray = await Promise.all(
+        paramsArray.map(async (params) => {
+          const commands = new GetObjectCommand(params);
+          const urls = await getSignedUrl(s3, commands, {
+            region: "ap-northeast-2",
+          });
+          return urls;
+        })
+      )
+
+      // // console.log("이곳", signedUrlsArray)
+      // const locationsWithSignedUrls = locationsWithDistance.map(
+      //   (location, index) => ({
+      //     ...location,
+      //     Posts: location.Posts.map((post, postIndex) => ({
+      //       ...post,
+      //       imgUrl: signedUrlsArray[postIndex],
+      //     })),
+      //   })
+      // );
+      // for (let i = 0; i < locationsWithDistance.length; i++) {
+      //   locationsWithDistance[i].imgUrl = signedUrlsArray[i];
+      // }
+
+      return res.status(200).json({ location });
+    } else {
+      return res.status(200).json({ location });
+    }
+
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 
