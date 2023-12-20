ff7947f99565815a85202849e2ff292dec3989ed
heyjk2212 <jinakim2212@gmail.com>
2023-12-20 11:28:45 +0900

    Update user router

--- src/routes/users/user.router.js
+++ src/routes/users/user.router.js
@@ -194,7 +194,7 @@ router.post("/verify-authentication-code", async (req, res, next) => {
     // 인증번호가 일치하는지 확인
     const checkVerificationCode = await prisma.verificationCode.findFirst({
       where: {
-        verificationCode: String(authenticationCode),
+        verificationCode: authenticationCode,
         email: email,
       },
     });
