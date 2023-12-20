923dd1ec21e4cd31cfc11af590ed383279b518fb
heyjk2212 <jinakim2212@gmail.com>
2023-12-20 19:58:07 +0900

    Update post router

--- src/routes/posts/posts.router.js
+++ src/routes/posts/posts.router.js
@@ -108,6 +108,8 @@ router.get("/posts", async (req, res, next) => {
       },
     });
 
+    console.log("하나", posts[0]);
+
     await getManyImagesS3(posts);
 
     // const cacheKey = `posts:${categoryName || 'all'}:${districtName || 'all'}`; //키를 ZADD로 표현합세
@@ -141,6 +143,7 @@ router.get("/posts/:postId", async (req, res, next) => {
         },
         Location: {
           select: {
+            locationId: true,
             address: true,
             storeName: true,
             latitude: true,
@@ -264,7 +267,7 @@ router.post(
               postCount: 1,
               Category: { connect: { categoryId: +category.categoryId } },
               District: { connect: { districtId: +district.districtId } },
-              User: { connect: { userId: +user.userId } },
+              User: { connect: { userId: +userId } },
             },
           });
 
@@ -290,7 +293,7 @@ router.post(
               content,
               star,
               likeCount: 0,
-              User: { connect: { userId: +user.userId } },
+              User: { connect: { userId: +userId } },
               Category: { connect: { categoryId: +category.categoryId } },
               Location: { connect: { locationId: +location.locationId } },
               imgUrl: imgNames.join(","),
@@ -411,6 +414,14 @@ router.delete("/posts/:postId", authMiddleware, async (req, res, next) => {
       await prisma.posts.delete({
         where: { postId: +postId },
       });
+    });
+    await prisma.locations.update({
+      where: { locationId: post.LocationId },
+      data: {
+        postCount: {
+          decrement: 1,
+        },
+      },
     });
 
     return res.status(200).json({ message: "게시글을 삭제하였습니다." });
