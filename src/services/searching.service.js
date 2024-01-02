0495874d94a912d52ece4c8b9d30c544a569aeef
jeongyy123 <jeongyy123@kakao.com>
2024-01-02 18:08:53 +0900

    update: layered architecture(searching.router)

---
+++ src/services/searching.service.js
@@ -0,0 +1,43 @@
+import { SearchingRepository } from '../repositories/searching.repository.js';
+
+export class SearchingService {
+  searchingRepository = new SearchingRepository();
+
+  /* 검색 기능 (유저, 가게 이름) */
+  getSearching = async (nickname, storeName) => {
+
+    const findUsers = await this.searchingRepository.findUsers(nickname);
+
+    if (!findUsers || findUsers.length === 0) {
+      const err = new Error(`검색하신 ${nickname} 님의 정보가 없어요.`);
+      err.statusCode = 404;
+      throw err;
+    }
+
+    let searchedData;
+    if (nickname) {
+      const seachedDataByNickname = await this.searchingRepository.getSearchingByNickname(findUsers);
+
+      if (!seachedDataByNickname || seachedDataByNickname.length === 0) {
+        const err = new Error(`해당 ${nickname} 님(이) 작성한 게시글이 없어요.`);
+        err.statusCode = 404;
+        throw err;
+      }
+      searchedData = seachedDataByNickname;
+    }
+
+    if (storeName) {
+      const seachedDataByStoreName = await this.searchingRepository.getSearchingByStoreName(storeName);
+
+      if (!seachedDataByStoreName || seachedDataByStoreName.length === 0) {
+        const err = new Error(`검색하신 ${storeName} 가게 정보가 없어요.`);
+        err.statusCode = 404;
+        throw err;
+      }
+      searchedData = seachedDataByStoreName;
+
+    }
+
+    return searchedData;
+  }
+}
