7a51dba8221dbe73fce174820796c03840d06bb7
jeongyy123 <jeongyy123@kakao.com>
2024-01-03 13:38:01 +0900

    fix: main.servcie refactoring

--- src/utils/getImageS3.js
+++ src/utils/getImageS3.js
@@ -2,7 +2,6 @@ import multer from "multer";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from "dotenv";
-import crypto from "crypto";
 
 dotenv.config();
 
